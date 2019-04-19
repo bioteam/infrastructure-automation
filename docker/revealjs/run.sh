c8aa817d5127e59a73b75e38eeb1dc90d6b9a40d
Karl Gutwin <karl@bioteam.net>
2019-04-19 08:22:33 -0400

    Add GNU screen to the demo terminal to enable second-screen terminal

--- docker/revealjs/run.sh
+++ docker/revealjs/run.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-(cd /demo; wetty -c /bin/bash) &
+screen -dmS demo -s /bin/bash
+
+cat <<EOF > /bin/dscreen.sh
+#!/bin/bash
+/usr/bin/screen -x demo
+EOF
+chmod a+x /bin/dscreen.sh
+
+(cd /demo; wetty -c /bin/dscreen.sh) &
 
 cd /reveal.js
 npm start
